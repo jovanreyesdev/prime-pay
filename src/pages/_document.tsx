1333c8dfa41b0927f54a4fc66d621cbb2937403a
Jovan <jovan.reyes.dev@gmail.com>
2023-03-30 18:48:00 +0800

    fix eslint build error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="PrimePay is an online payment application that provides a simple and secure payment gateway for making online payments using credit or debit cards. Developed by Jovan Reyes using React, NextJS, Typescript, and Tailwind CSS." />
         <meta name="keywords" content="PrimePay, online payment, payment gateway, React, NextJS, Typescript, Tailwind CSS" />
